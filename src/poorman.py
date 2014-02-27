766e7726ae938bfbbc3425d3e85adab6ec70b702
Sascha Spreitzer <sascha.spreitzer@meteoswiss.ch>
2014-02-27 17:36:34 +0100

    Initial commit

---
+++ src/poorman.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# encoding: utf-8
+'''
+    poorman - A poor man's cluster written in Python
+    Copyright (C) 2014   Swiss Confederation
+    Federal Department of Home Affairs FDHA
+    Federal Office of Meteorology and Climatology MeteoSwiss
+    
+    Sascha Spreitzer <sascha.spreitzer@meteoswiss.ch>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) alny later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+'''
+
+'''
+
+poormen -- A poorme
+
+poormen is a description
+
+It defines classes_and_methods
+
+@author:     user_name
+
+@copyright:  2014 organization_name. All rights reserved.
+
+@license:    license
+
+@contact:    user_email
+@deffield    updated: Updated
+'''
+
---
+++ src/poorman/__init__.py
@@ -0,0 +1,27 @@
+'''
+    poorman - A poor man's cluster written in Python
+    Copyright (C) 2014   Swiss Confederation
+    Federal Department of Home Affairs FDHA
+    Federal Office of Meteorology and Climatology MeteoSwiss
+    
+    Sascha Spreitzer <sascha.spreitzer@meteoswiss.ch>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) alny later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+'''
+
+from common import Common
+from config import Config
+from server import Server
+from client import Client
